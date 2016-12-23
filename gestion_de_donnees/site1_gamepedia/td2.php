3b734cb925961c771099bf9f32ad0e20d83f6346
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2016-12-23 21:32:11 +0100

    TD2

--- gestion_de_donnees/site1_gamepedia/td2.php
+++ gestion_de_donnees/site1_gamepedia/td2.php
@@ -19,7 +19,7 @@ Use app\models\Platform;
 $characters = Game::find(12342)->characters()->select('name', 'deck')->get();
 foreach($characters as $character){
   echo "<pre>"; var_dump($character); echo "</pre>";
-}*/
+}
 
 // les personnages des jeux dont le nom (du jeu) débute par 'Mario'
 $games = Game::where('name', 'like', '%Mario')->get();
@@ -27,6 +27,48 @@ foreach($games as $game){
   echo "<pre><h2>$game->name</h2></pre>";
   foreach($game->characters as $character){
     echo "<pre>$character->name<br /></pre>";
+  }
+}
+
+// les jeux développés par une compagnie dont le nom contient 'Sony'
+$companies = Company::where('name', 'like', '%Sony%')->get();
+foreach($companies as $company){
+  echo "<h4>$company->name</h4>";
+  foreach($company->games as $game){
+    echo "<pre>$game->name<br /></pre>";
+  }
+}
+
+// le rating initial (indiquer le rating board) des jeux dont le nom contient Mario
+$games = Game::where('name', 'like', '%Mario%')->get();
+foreach($games as $game){
+  echo "<h2>$game->name</h2>";
+  $ratings = $game->ratings;
+  foreach($ratings as $rating){
+    echo "<h4>$rating->name</h4>";
+    $rating_board = RatingBoard::find($rating->rating_board_id);
+    echo "$rating_board->name<br />";
+  }
+}
+
+// les jeux dont le nom débute par Mario et ayant plus de 3 personnages
+$games = Game::where('name', 'like', 'Mario%')->get();
+foreach($games as $game){
+  $characters_number = $game->characters->count();
+  if($characters_number > 3){
+    echo "<h2>$game->name</h2>";
+    foreach($game->characters as $character){
+      echo "$character->name<br />";
+    }
+  }
+}*/
+
+// les jeux dont le nom débute par Mario et dont le rating initial contient "3+"
+$games = Game::where('name', 'like', 'Mario%')->get();
+foreach($games as $game){
+  $ratings = $game->ratings->where('name', 'like', '%3+%');
+  foreach($ratings as $rating){
+    echo "$rating->name<br />";
   }
 }
 
