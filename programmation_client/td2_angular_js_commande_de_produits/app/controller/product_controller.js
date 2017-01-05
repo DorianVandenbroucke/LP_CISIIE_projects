c147d3ae380cc5d3215a5d93e05a182e2760af82
vandenbr3u <dorian.vandenbroucke@outlook.com>
2017-01-05 11:16:05 +0100

    json

--- programmation_client/td2_angular_js_commande_de_produits/app/controller/product_controller.js
+++ programmation_client/td2_angular_js_commande_de_produits/app/controller/product_controller.js
@@ -2,31 +2,19 @@
 angular.module("shop").controller(
   "ProductController",
   [
-    "$scope",
-    function($scope){
+    "$scope", "$http",
+    function($scope, $http){
 
-      $scope.products = [
-                          {
-                          name: 'atmega328',
-                          description: 'atmel microcontroller as used in arduino',
-                          price: '2€'
-                          },
-                          {
-                            name: 'attiny85',
-                            description: 'one of the smallest atmel microcontroller',
-                            price: '1€'
-                          },
-                          {
-                            name: 'esp8266',
-                            description: 'microcontroller for wifi with a tcp/ip stack',
-                            price: '3€'
-                          },
-                          {
-                            name: 'cortex-m4',
-                            description: '32 bits ARM processor',
-                            price: '4€'
-                          }
-                        ];
+      $http.get("json/products.php").then(function($response){
+        $scope.products = [];
+        $response.data.forEach(function(p){
+          $scope.products.push(p);
+        });
+      },
+      function(error){
+        console.log(error);
+      }
+    );
 
     }
   ]
